b744c6b00d1de17bb81fa48f45399ae5025e7a3e
Elizabeth Craig <elcraig@microsoft.com>
2022-07-21 17:57:34 -0700

    Add more logging to CI (#1)

--- scripts/fixRepoRefs.js
+++ scripts/fixRepoRefs.js
@@ -7,7 +7,7 @@ const defaultRepo = 'ecraig12345/renovate-config';
 
 const configFiles = glob.sync('./*.json5', { dot: false });
 
-// fix references in config files
+// fix repo references in config files to reflect the repo/branch being tested
 const headRef = /** @type {string} */ (process.env.GITHUB_HEAD_REF);
 const repo = /** @type {string} */ (process.env.GITHUB_REPOSITORY);
 if (headRef !== 'main' || repo !== defaultRepo) {
